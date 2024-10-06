8b160087359290607d74270c6a99853d6c0552de
Midnightess <asm1@bk.ru>
2024-10-06 18:29:38 +0300

    Добавила моки

---
+++ src/mocks/offers.ts
@@ -0,0 +1,306 @@
+import { Offers } from '../types/offers';
+
+
+const offersMock: Offers = [
+  {
+    city: {
+      name: 'Dusseldorf',
+      location: {
+        latitude: 51.225402,
+        longitude: 6.776314,
+        zoom: 13
+      }
+    },
+
+    previewImage: 'https://14.react.pages.academy/static/offer/3.jpg',
+    images: [
+      'https://14.react.pages.academy/static/offer/13.jpg',
+      'https://14.react.pages.academy/static/offer/6.jpg',
+      'https://14.react.pages.academy/static/offer/11.jpg',
+      'https://14.react.pages.academy/static/offer/16.jpg',
+      'https://14.react.pages.academy/static/offer/14.jpg',
+      'https://14.react.pages.academy/static/offer/9.jpg',
+      'https://14.react.pages.academy/static/offer/2.jpg',
+      'https://14.react.pages.academy/static/offer/15.jpg',
+      'https://14.react.pages.academy/static/offer/7.jpg',
+      'https://14.react.pages.academy/static/offer/17.jpg',
+      'https://14.react.pages.academy/static/offer/4.jpg',
+      'https://14.react.pages.academy/static/offer/8.jpg',
+      'https://14.react.pages.academy/static/offer/10.jpg',
+      'https://14.react.pages.academy/static/offer/5.jpg'
+    ],
+
+    title: 'Waterfront with extraordinary view',
+    isFavorite: false,
+    isPremium: false,
+    rating: 4.8,
+    type: 'room',
+    bedrooms: 1,
+    maxAdults: 2,
+    price: 142,
+    goods: [
+      'Laptop friendly workspace',
+      'Breakfast'
+    ],
+
+    host: {
+      id: 25,
+      name: 'Angelina',
+      isPro: true,
+      avatarUrl: 'img/avatar-angelina.jpg'
+    },
+
+    description: 'A new spacious villa, one floor. All commodities, jacuzzi and beautiful scenery. Ideal for families or friends.',
+    location: {
+      latitude: 51.237402,
+      longitude: 6.779314,
+      zoom: 16
+    },
+    id: crypto.randomUUID()
+  },
+  {
+    city: {
+      name: 'Amsterdam',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+
+    previewImage: 'https://14.react.pages.academy/static/offer/14.jpg',
+    images: [
+      'https://14.react.pages.academy/static/offer/8.jpg',
+      'https://14.react.pages.academy/static/offer/7.jpg',
+      'https://14.react.pages.academy/static/offer/17.jpg',
+      'https://14.react.pages.academy/static/offer/16.jpg',
+      'https://14.react.pages.academy/static/offer/1.jpg',
+      'https://14.react.pages.academy/static/offer/14.jpg',
+      'https://14.react.pages.academy/static/offer/12.jpg',
+      'https://14.react.pages.academy/static/offer/2.jpg',
+      'https://14.react.pages.academy/static/offer/10.jpg',
+      'https://14.react.pages.academy/static/offer/6.jpg',
+      'https://14.react.pages.academy/static/offer/20.jpg',
+      'https://14.react.pages.academy/static/offer/4.jpg',
+      'https://14.react.pages.academy/static/offer/5.jpg',
+      'https://14.react.pages.academy/static/offer/13.jpg'
+    ],
+
+    title: 'House in countryside',
+    isFavorite: true,
+    isPremium: false,
+    rating: 2.8,
+    type: 'room',
+    bedrooms: 1,
+    maxAdults: 2,
+    price: 144,
+
+    goods: [
+      'Laptop friendly workspace',
+      'Breakfast',
+      'Washer'
+    ],
+
+    host: {
+      id: 25,
+      name: 'Angelina',
+      isPro: true,
+      avatarUrl: 'img/avatar-angelina.jpg'
+    },
+
+    description: 'This is a place for dreamers to reset, reflect, and create. Designed with a slow pace in mind, our hope is that you enjoy every part of your stay; from making local coffee by drip in the morning, choosing the perfect record to put on as the sun sets.',
+    location: {
+      latitude: 52.385540000000006,
+      longitude: 4.902976,
+      zoom: 16
+    },
+
+    id: crypto.randomUUID()
+  },
+  {
+    city: {
+      name: 'Brussels',
+      location: {
+        latitude: 50.846557,
+        longitude: 4.351697,
+        zoom: 13
+      }
+    },
+
+    previewImage: 'https://14.react.pages.academy/static/offer/3.jpg',
+    images: [
+      'https://14.react.pages.academy/static/offer/9.jpg',
+      'https://14.react.pages.academy/static/offer/3.jpg',
+      'https://14.react.pages.academy/static/offer/5.jpg',
+      'https://14.react.pages.academy/static/offer/18.jpg',
+      'https://14.react.pages.academy/static/offer/8.jpg',
+      'https://14.react.pages.academy/static/offer/10.jpg',
+      'https://14.react.pages.academy/static/offer/1.jpg',
+      'https://14.react.pages.academy/static/offer/19.jpg',
+      'https://14.react.pages.academy/static/offer/16.jpg',
+      'https://14.react.pages.academy/static/offer/7.jpg',
+      'https://14.react.pages.academy/static/offer/6.jpg',
+      'https://14.react.pages.academy/static/offer/14.jpg',
+      'https://14.react.pages.academy/static/offer/4.jpg',
+      'https://14.react.pages.academy/static/offer/12.jpg'
+    ],
+
+    title: 'Loft Studio in the Central Area',
+    isFavorite: false,
+    isPremium: false,
+    rating: 2.9,
+    type: 'room',
+    bedrooms: 1,
+    maxAdults: 2,
+    price: 257,
+    goods: [
+      'Washing machine',
+      'Laptop friendly workspace',
+      'Towels',
+      'Baby seat',
+      'Air conditioning',
+      'Coffee machine',
+      'Fridge',
+      'Breakfast',
+      'Washer',
+      'Dishwasher'
+    ],
+
+    host: {
+      id: 25,
+      name: 'Angelina',
+      isPro: true,
+      avatarUrl: 'img/avatar-angelina.jpg'
+    },
+
+    description: 'I am happy to welcome you to my apartment in the city center! Three words: location, cosy and chic!',
+    location: {
+      latitude: 50.852557,
+      longitude: 4.3376969999999995,
+      zoom: 16
+    },
+    id: crypto.randomUUID()
+  },
+  {
+    city: {
+      name: 'Amsterdam',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+
+    previewImage: 'https://14.react.pages.academy/static/offer/6.jpg',
+    images: [
+      'https://14.react.pages.academy/static/offer/15.jpg',
+      'https://14.react.pages.academy/static/offer/18.jpg',
+      'https://14.react.pages.academy/static/offer/3.jpg',
+      'https://14.react.pages.academy/static/offer/6.jpg',
+      'https://14.react.pages.academy/static/offer/19.jpg',
+      'https://14.react.pages.academy/static/offer/2.jpg',
+      'https://14.react.pages.academy/static/offer/17.jpg',
+      'https://14.react.pages.academy/static/offer/10.jpg',
+      'https://14.react.pages.academy/static/offer/1.jpg',
+      'https://14.react.pages.academy/static/offer/4.jpg',
+      'https://14.react.pages.academy/static/offer/16.jpg',
+      'https://14.react.pages.academy/static/offer/20.jpg',
+      'https://14.react.pages.academy/static/offer/13.jpg',
+      'https://14.react.pages.academy/static/offer/5.jpg'
+    ],
+
+    title: 'Nice, cozy, warm big bed apartment',
+    isFavorite: false,
+    isPremium: false,
+    rating: 3.2,
+    type: 'hotel',
+    bedrooms: 4,
+    maxAdults: 4,
+    price: 330,
+    goods: [
+      'Breakfast',
+      'Washer',
+      'Laptop friendly workspace',
+      'Air conditioning'
+    ],
+
+    host: {
+      id: 25,
+      name: 'Angelina',
+      isPro: true,
+      avatarUrl: 'img/avatar-angelina.jpg'
+    },
+
+    description: 'Design interior in most sympathetic area! Complitely renovated, well-equipped, cosy studio in idyllic, over 100 years old wooden house. Calm street, fast connection to center and airport.',
+    location: {
+      latitude: 52.361540000000005,
+      longitude: 4.883976,
+      zoom: 16
+    },
+    id: crypto.randomUUID()
+  },
+  {
+    city: {
+      name: 'Hamburg',
+      location: {
+        latitude: 53.550341,
+        longitude: 10.000654,
+        zoom: 13
+      }
+    },
+
+    previewImage: 'https://14.react.pages.academy/static/offer/20.jpg',
+    images: [
+      'https://14.react.pages.academy/static/offer/20.jpg',
+      'https://14.react.pages.academy/static/offer/18.jpg',
+      'https://14.react.pages.academy/static/offer/13.jpg',
+      'https://14.react.pages.academy/static/offer/8.jpg',
+      'https://14.react.pages.academy/static/offer/16.jpg',
+      'https://14.react.pages.academy/static/offer/12.jpg',
+      'https://14.react.pages.academy/static/offer/6.jpg',
+      'https://14.react.pages.academy/static/offer/7.jpg',
+      'https://14.react.pages.academy/static/offer/9.jpg',
+      'https://14.react.pages.academy/static/offer/3.jpg',
+      'https://14.react.pages.academy/static/offer/17.jpg',
+      'https://14.react.pages.academy/static/offer/2.jpg',
+      'https://14.react.pages.academy/static/offer/15.jpg',
+      'https://14.react.pages.academy/static/offer/1.jpg'
+    ],
+
+    title: 'The house among olive',
+    isFavorite: true,
+    isPremium: false,
+    rating: 4.4,
+    type: 'house',
+    bedrooms: 3,
+    maxAdults: 10,
+    price: 360,
+    goods: [
+      'Baby seat',
+      'Laptop friendly workspace',
+      'Fridge',
+      'Air conditioning',
+      'Breakfast',
+      'Towels',
+      'Washer'
+    ],
+
+    host: {
+      id: 25,
+      name: 'Angelina',
+      isPro: true,
+      avatarUrl: 'img/avatar-angelina.jpg'
+    },
+
+    description: 'Peaceful studio in the most wanted area in town. Quiet house Near of everything. Completely renovated. Lovely neighbourhood, lot of trendy shops, restaurants and bars in a walking distance.',
+    location: {
+      latitude: 53.570341000000006,
+      longitude: 9.975654,
+      zoom: 16
+    },
+    id: crypto.randomUUID()
+  }
+];
+
+
+export {offersMock};
---
+++ src/types/offers.ts
@@ -0,0 +1,44 @@
+type OfferHost = {
+  id: number;
+  name: string;
+  isPro: boolean;
+  avatarUrl: string;
+}
+
+type OfferLocation = {
+  latitude:number;
+  longitude: number;
+  zoom: number;
+}
+
+type OfferCityLocation = {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+}
+
+type OfferCity = {
+  name: string;
+  location :OfferCityLocation;
+}
+
+export type Offer = {
+city: OfferCity;
+previewImage: string;
+images: string[];
+title: string;
+isFavorite:boolean;
+isPremium:boolean;
+rating: number;
+type: string;
+bedrooms: number;
+maxAdults: number;
+price:number;
+goods: string[];
+host: OfferHost;
+description:string;
+location: OfferLocation;
+id:string;
+}
+
+export type Offers = Offer[];
